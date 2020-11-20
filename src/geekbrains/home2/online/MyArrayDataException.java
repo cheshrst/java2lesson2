dfecf9567dc2e8924d40c90e256203896e36d9d6
cheshrst <seva1999@yandex.ru>
2020-11-20 19:49:26 +0300

    Initial commit

---
+++ src/geekbrains/home2/online/MyArrayDataException.java
@@ -0,0 +1,13 @@
+package geekbrains.home2.online;
+
+public class MyArrayDataException extends Exception {
+    public int i;
+    public int j;
+
+    MyArrayDataException(int i, int j) {
+        this.i = i;
+        this.j = j;
+
+    }
+
+}
---
+++ src/geekbrains/home2/online/MyArraySizeException.java
@@ -0,0 +1,6 @@
+package geekbrains.home2.online;
+
+public class MyArraySizeException extends Exception {
+    MyArraySizeException() {
+    }
+}
